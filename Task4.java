1099a4f5586c9e61087068a2c222c52f98a212b6
vlad <vladislavludinov3006@gmail.com>
2023-08-31 18:48:37 +0300

    Task4 minor updated

--- Task4.java
+++ Task4.java
@@ -3,35 +3,12 @@ import java.util.Scanner;
 public class Task4 {
     
     public static void main(String[] args) {
-        
-    //     Scanner scanner = new Scanner(System.in);
-
-    //     String str = scanner.nextLine();
-    //     String str2 = "       \n     ";
-    //     System.out.println("-------------");
-    //     System.out.println(str);
-    //     System.out.println("-------------");
-    //     System.out.println(str2);
-    //     System.out.println("-------------");
-    //     if (str2.isEmpty()) {
-    //         System.out.println("yes");
-    //     } else {
-    //         System.out.println("no");
-    //     }
-    //     str2 = str2.trim();
-    //     System.out.println(str2);
-    //     System.out.println("-------------");
-    //     if (str2.isEmpty()) {
-    //         System.out.println("yes");
-    //     } else {
-    //         System.out.println("no");
-    //     }
         inputEmptyString();
     }
 
     public static void inputEmptyString() {
+        Scanner scanner = new Scanner(System.in);
         try {
-            Scanner scanner = new Scanner(System.in);
             String newStr = scanner.nextLine();
             if (newStr.trim().isEmpty()) {
                 throw new RuntimeException("Input empty string");
@@ -39,6 +16,8 @@ public class Task4 {
         } catch (RuntimeException e) {
             e.printStackTrace();
             System.out.println("Нельзя вводить пустые строки");
+        } finally {
+            scanner.close();
         }
     }
 }
