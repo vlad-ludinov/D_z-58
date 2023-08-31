ae33dde12420fa203c621ee534e45a3c37279d01
vlad <vladislavludinov3006@gmail.com>
2023-08-31 16:48:09 +0300

    Task1 completed

--- Task1.java
+++ Task1.java
@@ -1,4 +1,31 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Task1 {
 
-    
+    public static void main(String[] args) {
+        
+        inputFloat();
+        
+    }
+
+
+    // я не совсем понимаю почему, ему подходит
+    // только дробное число через запятую,
+    // а не через точку, но все работает
+
+    public static void inputFloat() {
+        System.out.print("Пожалуйста введите дробное число: ");
+        Float fl = null;
+        while(fl == null) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                fl = scanner.nextFloat();
+                System.out.println("Введенное число: " + fl);
+            } catch (InputMismatchException e) {
+                System.out.print("Пожалуйста введите дробное число: ");
+            }
+        }
+            
+    }
 }
