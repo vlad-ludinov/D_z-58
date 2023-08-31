b726ee65d7b002ebb3bc53950e674c0f19203c6d
vlad <vladislavludinov3006@gmail.com>
2023-08-31 17:28:28 +0300

    Task3 completed

--- Task3.java
+++ Task3.java
@@ -1,3 +1,24 @@
+import java.io.FileNotFoundException;
+
 public class Task3 {
-    
+    public static void main(String[] args) {
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(207, 234);
+            int[] abc = {1, 2, 3, 4};
+            abc[3] = 9;
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+    public static void printSum(Integer a, Integer b) {
+        System.out.println(a + b);
+    }
+
 }
